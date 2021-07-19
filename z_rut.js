9e448d434f831020cb05744380d2f520683cc169
PhamNhatKhanh <khanhhcm4@gmail.com>
2021-07-20 06:59:45 +0700

    push code

---
+++ z_rut.js
@@ -0,0 +1,14 @@
+/*
+POSITION:
+*Top:
+ - push Div up to: not essential change div become positon detached flow. Simple use MARGIN-TOP:-'pixcel'
+
+CENTER:
+ - in Div width define height,width: format img center in div.
+position: relative;
+top: 50%;
+transform: translateY(-50%);
+but i don't know how do they action??
+
+
+ */
